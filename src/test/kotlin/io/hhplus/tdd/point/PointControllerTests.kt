2aeca4a7c66bc4b9a9280cd331298c09aeb03dbf
성승현 <ite476@gmail.com>
2025-07-08 23:04:00 +0900

    test: PointController에 정의된 엔드포인트 테스트 케이스 추가
    > 컨트롤러 액션은 항상 `ResponseEntity<T>` 반환하는 것으로 가정
    > kotest 기반으로 테스트 클래스 개편
    > 구현 테스트 케이스 목록
    > - `포인트 조회 - 정상 조회 (GET /point/{id})`
    > - `포인트 변경 내역 조회 - 정상 조회 (GET /point/{id}/histories)`
    > - `포인트 충전 - 정상 충전 (PATCH /point/{id}/charge)`
    > - `포인트 사용 - 정상 사용 (PATCH /point/{id}/use)`

--- src/test/kotlin/io/hhplus/tdd/point/PointControllerTests.kt
+++ src/test/kotlin/io/hhplus/tdd/point/PointControllerTests.kt
@@ -1,53 +1,117 @@
 package io.hhplus.tdd.point
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import org.assertj.core.api.Assertions.assertThat
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.http.MediaType
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 
-@WebMvcTest(PointController::class)
 class PointControllerTests {
-
-    @Autowired
-    private lateinit var mockMvc: MockMvc
-
-    @Autowired
-    private lateinit var objectMapper: ObjectMapper
-
     @Test
     fun `포인트 조회 - 정상 조회`() {
         // given
         val userId = 1L
-        val expectedPoint = 1000L
+        val expectedUserPoint = UserPoint(
+            userId,
+            1000L,
+            111L,
+        )
 
-        // when & then
-        mockMvc.perform(get("/point/$userId"))
-            .andExpect(status().isOk)
-            .andExpect(jsonPath("$.id").value(userId))
-            .andExpect(jsonPath("$.point").value(expectedPoint))
-            .andExpect(jsonPath("$.updateMillis").exists())
+        val mockPointService = mockk<PointService>()
+        every { mockPointService.getUserPoint(userId) } returns expectedUserPoint
+
+        val sutController = PointController(mockPointService)
+
+        // when
+        val response: ResponseEntity<UserPoint> = sutController.point(userId)
+
+        // then
+        response.statusCode shouldBe HttpStatus.OK
+        response.body shouldBe expectedUserPoint
+    }
+
+    @Test
+    fun `포인트 변경 내역 조회 - 정상 조회`(){
+        // given
+        val userId = 1L
+        val expectedHistory = listOf(
+            PointHistory(
+                id = 1L,
+                userId = userId,
+                type = TransactionType.CHARGE,
+                amount = 15_000L,
+                timeMillis = 1_000_000L
+            ),
+            PointHistory(
+                id = 2L,
+                userId = userId,
+                type = TransactionType.USE,
+                amount = 5_000L,
+                timeMillis = 1_100_000L
+            ),
+        )
+
+        val mockPointService = mockk<PointService>()
+        every { mockPointService.getPointHistories(userId) } returns expectedHistory
+
+        val sutController = PointController(mockPointService)
+
+        // when
+        val response = sutController.history(userId)
+
+        // then
+        response.statusCode shouldBe HttpStatus.OK
+        response.body shouldBe expectedHistory
     }
 
     @Test
     fun `포인트 충전 - 정상 충전`() {
         // given
         val userId = 1L
-        val amount = 1000L
+        val pointInput = 1_000L
+        val expectedUserPoint = UserPoint(
+            id = userId,
+            point = pointInput,
+            updateMillis = 1_000_000L
+        )
 
-        // when & then
-        mockMvc.perform(patch("/point/$userId/charge")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(objectMapper.writeValueAsString(amount)))
-            .andExpect(status().isOk)
-            .andExpect(jsonPath("$.id").value(userId))
-            .andExpect(jsonPath("$.point").value(amount))
-            .andExpect(jsonPath("$.updateMillis").exists())
+        val mockPointService = mockk<PointService>()
+        every { mockPointService.chargePoint(userId, pointInput) } returns expectedUserPoint
+
+        val sutController = PointController(mockPointService)
+
+        // when
+        val response = sutController.charge(userId, pointInput)
+
+        // then
+        response.statusCode shouldBe HttpStatus.OK
+        response.body shouldBe expectedUserPoint
     }
-} 
+
+    @Test
+    fun `포인트 사용 - 정상 사용`() {
+        // given
+        val userId = 1L
+        val pointBalance = 10_000L
+        val pointUsage = 5_000L
+
+        val expectedUserPoint = UserPoint(
+            id = userId,
+            point = pointBalance - pointUsage,
+            updateMillis = 1_000_000L
+        )
+
+        val mockPointService = mockk<PointService>()
+        every { mockPointService.usePoint(userId, pointUsage) } returns expectedUserPoint
+
+        val sutController = PointController(mockPointService)
+
+        // when
+        val response = sutController.use(userId, pointUsage)
+
+        // then
+        response.statusCode shouldBe HttpStatus.OK
+        response.body shouldBe expectedUserPoint
+    }
+}
