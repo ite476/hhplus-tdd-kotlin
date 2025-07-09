cb9194ab649ae321583e9bb9669bf40d0ec22e1a
성승현 <ite476@gmail.com>
2025-07-09 23:08:08 +0900

    test: `PointController` 관련 에러케이스 테스트 코드 추가
    > 이미 있던 테스트 케이스들에 대한 에러케이스 정의
    > `PointServiceException 과 그 수하들` 패키지에 관련 서비스 예외 정의

--- src/test/kotlin/io/hhplus/tdd/point/PointControllerTests.kt
+++ src/test/kotlin/io/hhplus/tdd/point/PointControllerTests.kt
@@ -1,11 +1,11 @@
 package io.hhplus.tdd.point
 
+import io.hhplus.tdd.point.`PointServiceException 과 그 수하들`.*
 import io.kotest.matchers.shouldBe
-import io.mockk.every
-import io.mockk.mockk
-import org.junit.jupiter.api.Test
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
+import io.mockk.*
+import org.junit.jupiter.api.*
+import org.springframework.http.*
+import org.springframework.web.server.ResponseStatusException
 
 class PointControllerTests {
     @Test
@@ -31,6 +31,26 @@ class PointControllerTests {
         // then
         response.statusCode shouldBe HttpStatus.OK
         response.body shouldBe expectedUserPoint
+    }
+
+    @Test
+    fun `포인트 조회 - 사용자 없음`() {
+        val userId = 1L
+
+        val mockPointService = mockk<PointService>()
+        every {
+            mockPointService.getUserPoint(userId)
+        } throws UserNotFoundException()
+
+        val sutController = PointController(mockPointService)
+
+        // when & then
+        val caughtException = assertThrows<ResponseStatusException> {
+            sutController.point(userId)
+        }
+
+        caughtException.statusCode shouldBe HttpStatus.CONFLICT
+        caughtException.body.detail shouldBe "회원이 존재하지 않습니다."
     }
 
     @Test
@@ -70,6 +90,26 @@ class PointControllerTests {
     }
 
     @Test
+    fun `포인트 변경 내역 조회 - 사용자 없음`() {
+        val userId = 1L
+
+        val mockPointService = mockk<PointService>()
+        every {
+            mockPointService.getPointHistories(userId)
+        } throws UserNotFoundException()
+
+        val sutController = PointController(mockPointService)
+
+        // when & then
+        val caughtException = assertThrows<ResponseStatusException> {
+            sutController.history(userId)
+        }
+
+        caughtException.statusCode shouldBe HttpStatus.CONFLICT
+        caughtException.body.detail shouldBe "회원이 존재하지 않습니다."
+    }
+
+    @Test
     fun `포인트 충전 - 정상 충전`() {
         // given
         val userId = 1L
@@ -93,6 +133,48 @@ class PointControllerTests {
         // then
         response.statusCode shouldBe HttpStatus.OK
         response.body shouldBe expectedUserPoint
+    }
+
+    @Test
+    fun `포인트 충전 - 사용자 없음`() {
+        val userId = 1L
+        val pointInput = 999_999_999L
+
+        val mockPointService = mockk<PointService>()
+        every {
+            mockPointService.chargePoint(userId, pointInput)
+        } throws UserNotFoundException()
+
+        val sutController = PointController(mockPointService)
+
+        // when & then
+        val caughtException = assertThrows<ResponseStatusException> {
+            sutController.charge(userId, pointInput)
+        }
+
+        caughtException.statusCode shouldBe HttpStatus.CONFLICT
+        caughtException.body.detail shouldBe "회원이 존재하지 않습니다."
+    }
+
+    @Test
+    fun `포인트 충전 - 음수 포인트 입력됨`() {
+        val userId = 1L
+        val pointInput = 999_999_999L
+
+        val mockPointService = mockk<PointService>()
+        every {
+            mockPointService.chargePoint(userId, pointInput)
+        } throws NegativeChargeAmountException()
+
+        val sutController = PointController(mockPointService)
+
+        // when & then
+        val caughtException = assertThrows<ResponseStatusException> {
+            sutController.charge(userId, pointInput)
+        }
+
+        caughtException.statusCode shouldBe HttpStatus.UNPROCESSABLE_ENTITY
+        caughtException.body.detail shouldBe "포인트 충전은 0 이상의 정수만 가능합니다."
     }
 
     @Test
@@ -121,5 +203,69 @@ class PointControllerTests {
         // then
         response.statusCode shouldBe HttpStatus.OK
         response.body shouldBe expectedUserPoint
+    }
+
+    @Test
+    fun `포인트 사용 - 사용자 없음`() {
+        val userId = 1L
+        val pointInput = 999_999_999L
+
+        val mockPointService = mockk<PointService>()
+        every {
+            mockPointService.usePoint(userId, pointInput)
+        } throws UserNotFoundException()
+
+        val sutController = PointController(mockPointService)
+
+        // when & then
+        val caughtException = assertThrows<ResponseStatusException> {
+            sutController.use(userId, pointInput)
+        }
+
+        caughtException.statusCode shouldBe HttpStatus.CONFLICT
+        caughtException.body.detail shouldBe "회원이 존재하지 않습니다."
+    }
+
+    @Test
+    fun `포인트 사용 - 음수 포인트 사용 시도`() {
+        val userId = 1L
+        val pointInput = -999_999_999L
+
+        val mockPointService = mockk<PointService>()
+        every {
+            mockPointService.usePoint(userId, pointInput)
+        } throws NegativeUsageException()
+
+        val sutController = PointController(mockPointService)
+
+        // when & then
+        val caughtException = assertThrows<ResponseStatusException> {
+            sutController.use(userId, pointInput)
+        }
+
+        caughtException.statusCode shouldBe HttpStatus.UNPROCESSABLE_ENTITY
+        caughtException.body.detail shouldBe "포인트 사용은 0 이상의 정수만 가능합니다."
+    }
+
+    @Test
+    fun `포인트 사용 - 잔고를 초과하는 포인트 사용 시도`() {
+        val userId = 1L
+        val userPointBalance = 1_000L
+        val pointInput = 999_999_999L
+
+        val mockPointService = mockk<PointService>()
+        every {
+            mockPointService.usePoint(userId, pointInput)
+        } throws NotEnoughPointException()
+
+        val sutController = PointController(mockPointService)
+
+        // when & then
+        val caughtException = assertThrows<ResponseStatusException> {
+            sutController.use(userId, pointInput)
+        }
+
+        caughtException.statusCode shouldBe HttpStatus.CONFLICT
+        caughtException.body.detail shouldBe "사용할 수 있는 포인트가 부족합니다."
     }
 }
